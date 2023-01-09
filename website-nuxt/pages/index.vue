e788921fa2c893b69673966187a94575447a1fcd
jochen ade <jochen.ade@hotmail.com>
2023-01-09 16:53:26 +0100

    rollback

---
+++ website-nuxt/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <HomeStage />
+  <div class="page-container w-container">
+    <div class="content wf-section">
+      <div class="containerinner content">
+        <HomeBlog />
+        <div id="projects" class="projectsblock">
+          <!-- <ProjectsHome /> -->
+          <HomeWhatsApp />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
