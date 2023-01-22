ae21ab40c02cafce20fc943a1a72190ddf137db5
jochen ade <jochen.ade@hotmail.com>
2023-01-22 16:10:05 +0100

    Starting additional features single page

--- website-nuxt/pages/blog/[slug].vue
+++ website-nuxt/pages/blog/[slug].vue
@@ -46,4 +46,58 @@ const query = groq`*[_type == "post" && slug.current == $slug][0]{
 const { data } = useSanityQuery(query, {
   slug: slug,
 });
+
+// DAS ALTE STRAPI SCRIPT
+
+// const { id } = useRoute().params;
+// const config = useRuntimeConfig();
+// const currentSection = ref("");
+// const postDate = ref("");
+// const articleTitles = ref("");
+// // fetch data from strapi
+// const { data: post } = await useFetch(
+//   `${config.public.baseUrl}/api/posts/${id}?locale=all&populate=*`
+// );
+// onMounted(() => {
+//   // fetch additional data
+//   const getHeadlines = async () => {
+//     const res = await fetch(
+//       `${config.public.baseUrl}/api/posts/${id}?locale=all&populate=*`
+//     );
+//     const resdata = await res.json();
+//     const data = resdata.data;
+//     // date formatting
+//     const dateString = data.attributes.createdAt;
+//     const dateObject = new Date(dateString);
+//     const options = { month: "long", day: "numeric", year: "numeric" };
+//     postDate.value = dateObject.toLocaleDateString("en-UK", options);
+//     // add ids to headlines
+//     document
+//       .getElementById("content")
+//       .querySelectorAll("h2,h3")
+//       .forEach(function (heading, i) {
+//         heading.setAttribute("id", i);
+//       });
+//     // parse rich text for headlines
+//     const parser = new DOMParser();
+//     const parsedDocument = parser.parseFromString(
+//       data.attributes.content,
+//       "text/html"
+//     );
+//     articleTitles.value = parsedDocument.querySelectorAll("h2, h3");
+//   };
+//   getHeadlines();
+//   // highlight sidebar navi on scroll
+//   const observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//       const id = entry.target.getAttribute("id");
+//       if (entry.intersectionRatio > 0) {
+//         currentSection.value = entry.target.getAttribute("id");
+//       }
+//     });
+//   });
+//   document.querySelectorAll("h2, h3").forEach((section) => {
+//     observer.observe(section);
+//   });
+// });
 </script>
