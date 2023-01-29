da1b2fc045ebdf18c9c31180292af799eaf157eb
jochen ade <jochen.ade@hotmail.com>
2023-01-29 13:05:05 +0100

    removed softscroll as it caused issues

--- website-nuxt/layouts/default.vue
+++ website-nuxt/layouts/default.vue
@@ -7,13 +7,3 @@
     <Footer />
   </div>
 </template>
-
-<script setup>
-// get path
-
-// const fullPath = useRoute().path;
-// const firstLevelPath = fullPath.substring(1, 5);
-// const onBlogPage = ref(false);
-
-// onBlogPage.value = firstLevelPath === "blog";
-</script>
