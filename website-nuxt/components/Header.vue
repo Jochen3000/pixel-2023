eb6c90024cdc8bf60241cadbcab297970d25e2f0
jochen ade <jochen.ade@hotmail.com>
2023-01-31 15:57:05 +0100

    header i18n working

--- website-nuxt/components/Header.vue
+++ website-nuxt/components/Header.vue
@@ -1,15 +1,26 @@
 <template>
   <div>
     <header>
-      <div class="headerinner">
+      <div class="headerinner" v-if="$route.path.substring(0, 3) === '/en'">
+        <div class="logo">
+          <NuxtLink to="/en">
+            <img src="/images/logo-pixel-berlin.svg" width="187" height="44" />
+          </NuxtLink>
+        </div>
+        <div class="navidesktop">
+          <NuxtLink to="/" class="navi-link"> DE </NuxtLink>
+          <div class="current">EN</div>
+        </div>
+      </div>
+      <div class="headerinner" v-else>
         <div class="logo">
           <NuxtLink to="/">
             <img src="/images/logo-pixel-berlin.svg" width="187" height="44" />
           </NuxtLink>
         </div>
         <div class="navidesktop">
-          <NuxtLink to="/" class="navi-link w..current">DE</NuxtLink>
-          <NuxtLink to="/en" class="navi-link">EN</NuxtLink>
+          <div class="current">DE</div>
+          <NuxtLink to="/en" class="navi-link"> EN </NuxtLink>
         </div>
       </div>
     </header>
@@ -18,3 +29,8 @@
     </div>
   </div>
 </template>
+
+<script setup>
+// get locale
+const route = useRoute();
+</script>
