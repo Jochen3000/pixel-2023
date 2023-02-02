d98f8c2d320418a0441c856e8a89b7eb8f460549
jochen ade <jochen.ade@hotmail.com>
2023-02-02 15:01:52 +0100

    images more

--- website-nuxt/components/Projects/Description.vue
+++ website-nuxt/components/Projects/Description.vue
@@ -8,12 +8,12 @@
     </p>
     <div class="project-tags" v-if="project?.tags">
       <div class="tag" v-for="item in project.tags">
-        <img src="images/tag-icon.svg" alt="tag icon" class="skill-icon" />
+        <img src="/images/tag-icon.svg" alt="tag icon" class="skill-icon" />
         <div class="text-block-5">{{ item }}</div>
       </div>
     </div>
     <a :href="project.external" class="view-project w-inline-block"
-      ><img src="images/link-chevron.svg" alt="chevron" class="link-chevron" />
+      ><img src="/images/link-chevron.svg" alt="chevron" class="link-chevron" />
       <div class="linktext">{{ project.title }}</div>
     </a>
   </div>
