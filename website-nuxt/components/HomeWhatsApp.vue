0d0070a57033ea5b0fc5577776fcca86c7095d5d
jochen ade <jochen.ade@hotmail.com>
2023-02-08 14:21:20 +0100

    form 1001

--- website-nuxt/components/HomeWhatsApp.vue
+++ website-nuxt/components/HomeWhatsApp.vue
@@ -293,206 +293,12 @@
               </div>
             </div>
           </div>
+          <!-- form  -->
           <div class="whatsapp-bottom" v-if="$route.path === '/en'">
             <div class="message-box">
               <div class="form-block w-form">
                 <form
-                  name="form"
-                  method="POST"
-                  data-netlify="true"
-                  class="whatsapp-form"
-                >
-                  <div class="message-send">
-                    <input
-                      type="text"
-                      class="text-entry-name w-input"
-                      maxlength="200"
-                      name="Name"
-                      data-name="Name"
-                      placeholder=""
-                      id="name"
-                      required=""
-                    /><input
-                      type="text"
-                      class="text-entry-message w-input"
-                      maxlength="200"
-                      name="message"
-                      data-name="message"
-                      placeholder=""
-                      id="message-2"
-                      required=""
-                    /><input
-                      type="text"
-                      class="text-entry-contact w-input"
-                      maxlength="200"
-                      name="contact"
-                      data-name="contact"
-                      placeholder=""
-                      id="contact-2"
-                      required=""
-                    /><img
-                      alt="whatsapp submit button"
-                      width="24"
-                      height="24"
-                      src="/images/button-whatsapp.svg"
-                      loading="lazy"
-                      class="submit-arrow-button"
-                    />
-                  </div>
-                  <input
-                    type="submit"
-                    data-wait=""
-                    value="submit"
-                    class="submit-button w-button"
-                  />
-                </form>
-                <div class="success-message w-form-done">
-                  <div class="submit-success-message">ok</div>
-                </div>
-                <div class="w-form-fail">
-                  <div>error</div>
-                </div>
-              </div>
-            </div>
-            <div class="wipeup-container">
-              <div class="swipe-up"></div>
-            </div>
-          </div>
-          <div class="whatsapp-bottom" v-if="$route.path === '/en'">
-            <div class="message-box">
-              <div class="form-block w-form">
-                <form
-                  name="form"
-                  method="POST"
-                  data-netlify="true"
-                  class="whatsapp-form"
-                >
-                  <div class="message-send">
-                    <input
-                      type="text"
-                      class="text-entry-name w-input"
-                      maxlength="200"
-                      name="Name"
-                      data-name="Name"
-                      placeholder=""
-                      id="name"
-                      required=""
-                    /><input
-                      type="text"
-                      class="text-entry-message w-input"
-                      maxlength="200"
-                      name="message"
-                      data-name="message"
-                      placeholder=""
-                      id="message-2"
-                      required=""
-                    /><input
-                      type="text"
-                      class="text-entry-contact w-input"
-                      maxlength="200"
-                      name="contact"
-                      data-name="contact"
-                      placeholder=""
-                      id="contact-2"
-                      required=""
-                    /><img
-                      alt="whatsapp submit button"
-                      width="24"
-                      height="24"
-                      src="/images/button-whatsapp.svg"
-                      loading="lazy"
-                      class="submit-arrow-button"
-                    />
-                  </div>
-                  <input
-                    type="submit"
-                    data-wait=""
-                    value="submit"
-                    class="submit-button w-button"
-                  />
-                </form>
-                <div class="success-message w-form-done">
-                  <div class="submit-success-message">ok</div>
-                </div>
-                <div class="w-form-fail">
-                  <div>error</div>
-                </div>
-              </div>
-            </div>
-            <div class="wipeup-container">
-              <div class="swipe-up"></div>
-            </div>
-          </div>
-          <div class="whatsapp-bottom" v-if="$route.path === '/en'">
-            <div class="message-box">
-              <div class="form-block w-form">
-                <form
-                  name="form"
-                  method="POST"
-                  data-netlify="true"
-                  class="whatsapp-form"
-                >
-                  <div class="message-send">
-                    <input
-                      type="text"
-                      class="text-entry-name w-input"
-                      maxlength="200"
-                      name="Name"
-                      data-name="Name"
-                      placeholder=""
-                      id="name"
-                      required=""
-                    /><input
-                      type="text"
-                      class="text-entry-message w-input"
-                      maxlength="200"
-                      name="message"
-                      data-name="message"
-                      placeholder=""
-                      id="message-2"
-                      required=""
-                    /><input
-                      type="text"
-                      class="text-entry-contact w-input"
-                      maxlength="200"
-                      name="contact"
-                      data-name="contact"
-                      placeholder=""
-                      id="contact-2"
-                      required=""
-                    /><img
-                      alt="whatsapp submit button"
-                      width="24"
-                      height="24"
-                      src="/images/button-whatsapp.svg"
-                      loading="lazy"
-                      class="submit-arrow-button"
-                    />
-                  </div>
-                  <input
-                    type="submit"
-                    data-wait=""
-                    value="submit"
-                    class="submit-button w-button"
-                  />
-                </form>
-                <div class="success-message w-form-done">
-                  <div class="submit-success-message">ok</div>
-                </div>
-                <div class="w-form-fail">
-                  <div>error</div>
-                </div>
-              </div>
-            </div>
-            <div class="wipeup-container">
-              <div class="swipe-up"></div>
-            </div>
-          </div>
-          <div class="whatsapp-bottom" v-if="$route.path === '/en'">
-            <div class="message-box">
-              <div class="form-block w-form">
-                <form
-                  name="form"
+                  name="contact-en"
                   method="POST"
                   data-netlify="true"
                   class="whatsapp-form"
@@ -557,7 +363,7 @@
             <div class="message-box">
               <div class="form-block w-form">
                 <form
-                  name="form-en"
+                  name="contact-de"
                   method="POST"
                   data-netlify="true"
                   class="whatsapp-form"
@@ -635,15 +441,6 @@ function currentTime() {
   let minutesString = minutes <= 9 ? `0${minutes}` : `${minutes}`;
   return `${hours}:${minutesString}`;
 }
-
-function formID() {
-  if (route.path === "/en") {
-    return "form-en";
-  } else {
-    return "form-de";
-  }
-}
-formID();
 
 onMounted(() => {
   const nuxtApp = useNuxtApp();
